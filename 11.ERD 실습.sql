2484f5893910d6045ccdb2b9d3bd95885a8527c9
jominhyeong97 <jumbo0303@daum.net>
2025-05-30 18:56:03 +0900

    5월 30일

--- 11.ERD 실습.sql
+++ 11.ERD 실습.sql
@@ -5,17 +5,13 @@ age int not null, primary key(user_id));
 create table seller (seller_id bigint auto_increment, seller_num varchar(50) unique, name varchar(50) not null, 
 address varchar(50) not null, primary key (seller_id));
 
-create table goods (goods_id bigint auto_increment, goods_name varchar(50) not null unique, goods_num bigint unsigned not null,
-primary key(goods_id));
-
 create table orders (order_id bigint auto_increment, user_id bigint not null, order_time datetime default current_timestamp,
 primary key(order_id), 
 foreign key(user_id) references users(user_id));
 
-create table goods_list (list_id bigint auto_increment, seller_id bigint not null, goods_id bigint , 
-list_time datetime default current_timestamp, 
-primary key(list_id), foreign key (seller_id) references seller(seller_id), 
-foreign key (goods_id) references goods(goods_id));
+create table goods_list (list_id bigint auto_increment, seller_id bigint not null, 
+list_time datetime default current_timestamp, goods_name varchar(50), goods_qaun bigint unsigned, price bigint,
+primary key(list_id), foreign key (seller_id) references seller(seller_id));
 
 create table order_detail (detail_id bigint auto_increment, list_id bigint not null, 
 order_id bigint not null, detail_num bigint unsigned not null, 
@@ -58,15 +54,16 @@ select * from seller;
 
 -- 상품등록(판매자 정보가 나와야함) >
     DELIMITER //
-    create procedure goods_register(in goods_nameInput varchar(50), in goods_numInput bigint unsigned, in seller_idInput bigint)
+    create procedure goods_register(in seller_idInput bigint, in goods_nameInput varchar(50), 
+    in goods_qaunInput bigint unsigned, in priceInput bigint)
     begin
         declare exit handler for SQLEXCEPTION
         begin
             rollback;
         end;
         start transaction;
-        insert into goods(goods_name, goods_num) values (goods_nameInput, goods_numInput);
-        insert into goods_list(seller_id) values (seller_idInput);
+        insert into goods_list(seller_id, goods_name, goods_qaun, price) 
+        values (seller_idInput, goods_nameInput, goods_qaunInput, priceInput);
 
         commit;
     end //
@@ -74,8 +71,7 @@ select * from seller;
 
 -- 상품정보조회(재고, 품명 등등)
 
-select goods_list.seller_id, goods.goods_name, goods.goods_num, goods.goods_id from goods 
-left join goods_list on goods.goods_id = goods_list.goods_id; 
+select * from goods_list
 
 
 -- 상품 주문
@@ -89,8 +85,9 @@ left join goods_list on goods.goods_id = goods_list.goods_id;
         end;
         start transaction;
         insert into orders(user_id) values (user_idInput);
+        select id from orders order by id desc limit;
         insert into order_detail(list_id, order_id, detail_num) values (list_idInput, order_idInput, detail_numInput);
-        update goods set goods_num = goods_num - detail_numInput  where goods.goods_id = list_idInput;
+        update goods set goods_qaun = goods_qaun - detail_numInput  where goods_list.list_id = order_idInput ;
         commit;
     end //
     DELIMITER ;
